a3193096b421b8615f425cc433dae0d098cdf9b9
Alex Miranda <alex.miranda.sanchez@gmail.com>
2020-12-24 14:15:48 -0600

    Update Constants.groovy File

--- src/org/example/Constants.groovy
+++ src/org/example/Constants.groovy
@@ -1,4 +1,6 @@
-package org.exampleclass Constants {
-  static final String SLACK_MESSAGE = "Sending Slack Notification"
-  static final String EMAIL_MESSAGE = "Sending Email"
+package org.example
+
+class Constants {
+  public static final String SLACK_MESSAGE = "Sending Slack Notification..."
+  public static final String EMAIL_MESSAGE = "Sending Email..."
 }
--- vars/buildJavascriptApp.groovy
+++ vars/buildJavascriptApp.groovy
@@ -1,21 +1,21 @@
 def call(Map config=[:], Closure body) {
-	node {
-		git url: "https://github.com/werne2j/sample-nodejs"
+    node {
+        git url: "https://github.com/werne2j/sample-nodejs"
 
-		stage("Install App") {
-			sh "npm install"
-		}
+        stage("Install") {
+            sh "npm install"
+        }
 
-		stage("Test") {
-			sh "npm test"
-		}
+        stage("Test") {
+            sh "npm test"
+        }
 
-		stage ("Deploy App") {
-			if (config.deploy) {
-				sh "npm publish"
-			}
-		}
+        stage("Deploy") {
+            if (config.deploy) {
+                sh "npm publish"
+            }
+        }
 
-		body()
-	}
+        body()
+    }
 }
